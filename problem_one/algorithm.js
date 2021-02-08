ef8c5923887f94ca1470844dc3003d35050d5c7a
Andy Trachier <andrew.trachier@gmail.com>
2021-02-08 11:55:32 -0600

    part C begins

--- problem_one/algorithm.js
+++ problem_one/algorithm.js
@@ -1,8 +1,8 @@
-//Problem 1 part A//
+//Problem 1 part A
 function sumNum(x,y) { 
   return x+y;
 }
-// array for function output 
+// array for 3
 var num = [];
 // count by x, y times 
 function countBy(x,y) { 
@@ -19,7 +19,31 @@ function countBy(x,y) {
     console.log(n);
   }
 countBy(3,1000)
+
 // Problem 1 part B
 function bigNum (a,b) {
   return a+b;
 }
+//array for 5
+var aim = [];
+//count by a, b times
+function increase(a,b) {
+let m = 0;
+  for (O = 0; O < b; O++) 
+    {
+  m = bigNum(m,a)
+  aim.push(m)
+  }
+  aim.pop();
+  aim.forEach(sum); 
+  function sum(w) { 
+   m += w;
+    }
+    console.log(m);
+  }
+
+increase(5,1000)
+
+//Problem 1 part C
+
+  
