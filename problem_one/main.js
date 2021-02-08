ae9d8c7061bfee6a71bab5d520f0222816a467a0
Andy Trachier <andrew.trachier@gmail.com>
2021-02-07 19:20:26 -0600

    change to main.js

--- problem_one/main.js
+++ problem_one/main.js
@@ -3,11 +3,11 @@ function sumNum(x,y) {
   return x+y;
 }
 // count by x three times
-function countBy(x) {
+function countBy(x,y) {
   let n = 0;
-  for (i = 0; i < 3; i++ ) {
+  for (i = 0; i < y; i++ ) {
     n = sumNum(n,x);
     console.log(n);
   }
 }
-countBy(3)
+countBy(3,3)
