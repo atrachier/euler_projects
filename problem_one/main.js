75ceed4408d3d3bd8e24ab7c965d81ac48029fb2
Matt Trachier <matttrach@gmail.com>
2021-02-07 19:06:25 -0600

    count by three

--- problem_one/main.js
+++ problem_one/main.js
@@ -4,8 +4,10 @@ function sumNum(x,y) {
 }
 // count by x three times
 function countBy(x) {
+  let n;
   for (i = 0; i < 3; i++ ) {
-    console.log(sumNum(x,0));
+    n = sumNum(0,x);
+    console.log(n);
   }
 }
 countBy(3)
