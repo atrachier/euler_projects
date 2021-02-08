6ab6b48052feef2f0546b85c8e292ce8de09a497
Matt Trachier <matttrach@gmail.com>
2021-02-07 18:38:27 -0600

    ac

--- problem_one/main.js
+++ problem_one/main.js
@@ -4,7 +4,9 @@ function sumNum(x,y) {
 }
 // count by x three times
 function countBy(x) {
-  for (x= 0; x<10; x+3);
+  for (x = 0; x < 10; x + 3){
+    // do stuff here
+  }
   console.log(sumnum(x,0));
 }
 countBy(3)
