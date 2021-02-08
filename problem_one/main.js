633e6c27def60a2e033b7f2fb227873a7d7fb95b
Andy Trachier <andrew.trachier@gmail.com>
2021-02-07 22:37:31 -0600

    next step

--- problem_one/main.js
+++ problem_one/main.js
@@ -2,22 +2,19 @@
 function sumNum(x,y) { 
   return x+y;
 }
-// count by x, y times
+// array for function output 
+var num = [];
+// count by x, y times 
 function countBy(x,y) { 
-  var num = [];
   let n = 0;
   for (i = 0; i < y; i++ ) {
     n = sumNum(n,x);
     num.push(n);
     num.forEach(ttl); 
     function ttl(z) { 
-      n += z;
-    }
-
-      
+    n += z;
+  }  
     }
     console.log(num);
-  } 
-
-
+  }
 countBy(3,10)
