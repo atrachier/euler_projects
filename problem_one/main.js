38fe8b3f24b2e0009e29a8e1bf4cd06814ed78ec
Matt Trachier <matttrach@gmail.com>
2021-02-14 16:58:56 -0600

    fixing spacing

--- problem_one/main.js
+++ problem_one/main.js
@@ -2,6 +2,7 @@
 function sumNum(x,y) { 
   return x+y;
 }
+
 // array for 3
 var num = [];
 // count by x, y times 
@@ -11,36 +12,45 @@ function countBy(x,y) {
     n = sumNum(n,x);
     num.push(n);
   }  
+
   num.pop();
   num.forEach(ttl); 
+  
   function ttl(z) { 
-   n += z;
-    }
-    console.log(n);
+    n += z;
   }
+  console.log(n);
+}
+
 countBy(3,10)
 
 // Problem 1 part B
 function bigNum (x,y) {
   return x+y;
 }
+
 //array for 5
 var aim = [];
+
 //count by x, y times
 function increase(x,y) {
-let n = 0;
-  for (i = 0; i < y; i++) 
-    {
-  n = bigNum(n,x)
-  aim.push(n)
+
+  let n = 0;
+
+  for (i = 0; i < y; i++) {
+    n = bigNum(n,x)
+    aim.push(n)
   }
+
   aim.pop();
   aim.forEach(sum);
+
   function sum(z)  {
-   n += z;
-    }
-    console.log(n);
+    n += z;
   }
+
+  console.log(n);
+}
 
 increase(5,10)
 
