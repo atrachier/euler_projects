ff9821d20d2e990ee03d88395dde62d76e26eabb
Andy Trachier <andrew.trachier@gmail.com>
2021-02-14 19:44:09 -0600

    changes

--- problem_one/algorithm.js
+++ problem_one/algorithm.js
@@ -2,10 +2,17 @@
 exports.countBy = function (x,y,num) { 
   let n = 0;
   for (i = 0; i < y; i++ ) {
-    n += x;
+
+     if (y<1000) {
+             n = x % y
+     } 
+    else {
+             
+     }  
+    }
     num.push(n);
   }
-}
+
 
 exports.rdf = function(a,b) {
   return a+b;
