09a3cd8b84818c104b0c2d3c3f7622090ad33243
Andy Trachier <andrew.trachier@gmail.com>
2021-02-17 22:30:15 -0600

    final revision

--- problem_one/main.js
+++ problem_one/main.js
@@ -1,5 +1,6 @@
 const a = require('./algorithm.js')
 
-const partA = a.countBy(3, 5, 10)
-// console.log(partA)
-console.log(partA.reduce((a, b) => { return a + b }, 0))
+const partA = a.countBy(3, 5, 200)
+const partB = a.countBy(3, 0, 133)
+const partC = partA.concat(partB)
+console.log(partC.reduce((a, b) => { return a + b }, 0))
