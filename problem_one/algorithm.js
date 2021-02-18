bb0a94d8084e32388d3bc232ecf017c8a386b7d7
Andy Trachier <andrew.trachier@gmail.com>
2021-02-17 18:15:51 -0600

    fixes to algorithm and main.js

--- problem_one/algorithm.js
+++ problem_one/algorithm.js
@@ -1,14 +1,12 @@
 
-exports.countBy = function (x,y,num) { 
-  let n = 0;
-  for (i = 0; i < y; i++ ) {
-
-    n += x;
-    num.push(n);
+exports.countBy = function (x, y, num) {
+  let n = 0
+  for (i = 0; i < y; i++) {
+    n += x
+    num.push(n)
   }
 }
 
-exports.rdf = function(a,b) {
-  return a+b;
+exports.rdf = function (a, b) {
+  return a + b
 }
-
--- problem_one/main.js
+++ problem_one/main.js
@@ -1,9 +1,8 @@
-var a = require('./algorithm.js');
+const a = require('./algorithm.js')
 
-
-var num = [];
-a.countBy(3,10,num)
-a.countBy(5,10,num)
-var sum = num.reduce(a.rdf,0)
+const num = []
+a.countBy(3, 10, num)
+a.countBy(5, 10, num)
+const sum = num.reduce(a.rdf, 0)
 
 console.log(sum)
