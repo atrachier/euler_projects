c6e389605bd809b21f12c6a9cd89bca0ba5c28fb
Andy Trachier <andrew.trachier@gmail.com>
2021-02-17 21:23:48 -0600

    revision to main/algorithm

--- problem_one/algorithm.js
+++ problem_one/algorithm.js
@@ -1,9 +1,10 @@
 
-exports.countBy = (x, y) => {
+exports.countBy = (x,z,y) => {
   const num = []
   let n = 0
   for (let i = 0; i < y; i++) {
     n += x
+    n += z
     num.push(n)
   }
   return num
