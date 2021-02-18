8e56e2eb3c2a86b34f2574c91a40debedc6f3d20
Matt Trachier <matttrach@gmail.com>
2021-02-17 23:21:49 -0600

    adding solution to test

--- problem_one/test.sh
+++ problem_one/test.sh
@@ -2,6 +2,7 @@
 
 git pull
 A=$(node main.js)
-if [ "$A" = "395" ]; then echo "Math Passes"; else echo "Math Fails"; fi
+
+if [ "$A" = "267333" ]; then echo "Math Passes"; else echo "Math Fails"; fi
 if ./node_modules/.bin/eslint main.js; then echo "Main Lints"; else "Main Fails Lint"; fi
 if ./node_modules/.bin/eslint algorithm.js; then echo "Algorithm Lints"; else "Algorithm Fails Lint"; fi
