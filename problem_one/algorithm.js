831a99bf6a3ab0c96cb6d792588862d17c07708d
Andy Trachier <andrew.trachier@gmail.com>
2021-02-18 00:26:57 -0600

    Final revision

--- problem_one/algorithm.js
+++ problem_one/algorithm.js
@@ -2,11 +2,9 @@
 exports.countBy = function (x, y, n) {
   const num = []
   for (let i = 0; i < n; i++) {
-    
-    if (i % x == 0 || i % y == 0) {
+    if (i % x === 0 || i % y === 0) {
       num.push(i)
     }
-    
   }
   return num
 }
