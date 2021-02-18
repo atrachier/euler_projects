02e5307ea5d5960ee3946cb19f172b55cb2dac04
Andy Trachier <andrew.trachier@gmail.com>
2021-02-17 19:43:39 -0600

    revisions for algorithm.js

--- problem_one/algorithm.js
+++ problem_one/algorithm.js
@@ -1,9 +1,9 @@
-
+let num = []
 exports.countBy = function (x, y) {
   let n = 0
   for (let i = 0; i < y; i++) {
     n += x
     num.push(n)
-    return num
   }
+  return num
 }
--- problem_one/main.js
+++ problem_one/main.js
@@ -1,6 +1,5 @@
 const a = require('./algorithm.js')
 
-let num = num []
 a.countBy(3, 10, num)
 a.countBy(5, 10, num)
 
