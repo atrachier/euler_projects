010c164b72540b41418f83ae1224844ad2816b09
Andy Trachier <andrew.trachier@gmail.com>
2021-02-17 19:57:14 -0600

    revisions for num

--- problem_one/main.js
+++ problem_one/main.js
@@ -1,7 +1,6 @@
 const a = require('./algorithm.js')
 
-
 let partA = a.countBy(3, 10)
-a.countBy(5, 10)
+let partB = a.countBy(5, 10)
 
-console.log(num.reduce((a, b) => { return a + b }, 0))
+console.log(partA.reduce((a, b) => { return a + b }, 0))
