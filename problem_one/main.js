bb6893a035363c0c9ce5db4d9f7d052287bdf891
Matt Trachier <matttrach@gmail.com>
2021-02-17 18:38:17 -0600

    moving rdf to an inline function

--- problem_one/main.js
+++ problem_one/main.js
@@ -3,7 +3,5 @@ const a = require('./algorithm.js')
 const num = []
 a.countBy(3, 10, num)
 a.countBy(5, 10, num)
-const rdf = function (a, b) {
-  return a + b
-}
-console.log(num.reduce(rdf, 0))
+
+console.log(num.reduce((a, b) => { return a + b }, 0))
